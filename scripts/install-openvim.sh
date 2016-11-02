73940e09eb337f1039e7aac70030a6ff1f74f180
garciadeblas <gerardo.garciadeblas@telefonica.com>
2016-11-02 16:57:37 +0100

    install-openvim updated to use tag v1.0.1
    Change-Id: I87badf34dfb574bb1ac35977acb03db78d5f433a
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- scripts/install-openvim.sh
+++ scripts/install-openvim.sh
@@ -237,7 +237,7 @@ echo '
 #####                 DOWNLOAD SOURCE                       #####
 #################################################################'
 su $SUDO_USER -c 'git clone '"${GIT_URL}"' openvim'
-[[ -z $DEVELOP ]] && su $SUDO_USER -c 'cd openvim; git checkout tags/v1.0; cd ..'
+[[ -z $DEVELOP ]] && su $SUDO_USER -c 'git -C openvim checkout tags/v1.0.1'
 
 #Unncoment to use a concrete branch, if not main branch 
 #pushd openvim
